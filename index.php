4915850f3c77e0b32cff874dac10c2e19d77676e
Sara Machado <sarasanto1309@gmail.com>
2021-06-23 14:54:25 -0300

    TASK- 000: Adição de estilo
    Foi adicionado css embutido

--- index.php
+++ index.php
@@ -5,6 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jogo de Cartas</title>
+    <style>
+        body {
+           background-color: #99f2b1;
+           font-family: 'Verdana';
+       }
+       input{
+           background-color: #57ad6e;
+       }
+       button{
+        background-color: #57ad6e;
+        font-family: 'Verdana';
+       }
+       form{
+           padding-top: 50px;
+           padding-left: 550px;
+       }
+       select{
+        background-color: #57ad6e;
+        font-family: 'Verdana';
+       }
+       </style>
     <?php 
     $jogador = "";
     if(isset($_POST['nome']))
@@ -17,8 +38,6 @@
     $cartas = "";
        if(isset($_POST['cartas']))
           $cartas = $_POST['cartas'];
-
-    $ouro1 = '<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTry9XVMP2enUiSdmlaG5y091Dq5Mr389Mzze53Ng6G_CIaRENWyIAZmkSvIIdKo3-EzKw&usqp=CAU">';
     ?> 
 </head>
 <body>
