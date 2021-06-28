3c69fcad933c8a7da06a8bb976a470d181f88430
Sara Machado <sarasanto1309@gmail.com>
2021-06-28 14:34:42 -0300

    TASK-003: Adição de linhas de código e imagens
    Foi adicionado linhas de código, função e imagens(cartas)

--- cartas.php
+++ cartas.php
@@ -6,22 +6,143 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jogo de Cartas Simples</title>
 </head>
+     <style>
+        img{
+            width: 70px;
+        }
+     </style>
 <body>
-    <h2>Jogador:</h2>
     <?php
-    echo $_POST['nome'];
-    echo "<br>";
+    function sortear($num){
+        $Jogador = array();
+        for($i = 0; $i < $num; $i++){
+            $n = rand(1,13);
+        $Jogador[] = $n;
+    }
+    sort($Jogador);
+    return $Jogador;   
+    }
 
-    $cartas = "";
-    if(isset($_POST['cartas']))
-        $cartas = $_POST['cartas'];
-
-    $naipe = "";
-    if(isset($_POST['naipe']))
-        $naipe = $_POST['naipe'];
-
-    $Jogador = array();
-    for($i = 0; $i < $cartas; $i++){
+    function mostrarCartas($Jogador, $naipe){
+        if ($naipe == 1) {
+            echo "Ouros <br>";
+        }
+        if ($naipe ==2) {
+            echo "Copas <br>";
+        }
+        if ($naipe == 3) {
+            echo "Paus <br>";
+        }
+        if ($naipe == 4) {
+            echo "Espadas <br>";
+        }
+        foreach($Jogador as $c){
+            if ($naipe == 1 and $c == 1) {
+                echo "<img src='img/ouro1.png'> .\n";
+            }elseif($naipe == 1 and $c == 2) {
+                echo "<img src='img/ouro2.png'> .\n";
+            }elseif($naipe == 1 and $c == 3) {
+                echo "<img src='img/ouro3.png'> .\n";
+            }elseif($naipe == 1 and $c == 4) {
+                echo "<img src='img/ouro4.png'> .\n";
+            }elseif($naipe == 1 and $c == 5) {
+                echo "<img src='img/ouro5.png'> .\n";
+            }elseif($naipe == 1 and $c == 6) {
+                echo "<img src='img/ouro6.png'> .\n";
+            }elseif($naipe == 1 and $c == 7) {
+                echo "<img src='img/ouro7.png'> .\n";
+            }elseif($naipe == 1 and $c == 8) {
+                echo "<img src='img/ouro8.png'> .\n";
+            }elseif($naipe == 1 and $c == 9) {
+                echo "<img src='img/ouro9.png'> .\n";
+            }elseif($naipe == 1 and $c == 10) {
+                echo "<img src='img/ouro10.png'> .\n";
+            }elseif($naipe == 1 and $c == 11) {
+                echo "<img src='img/ouro11.png'> .\n";
+            }elseif($naipe == 1 and $c == 12) {
+                echo "<img src='img/ouro12.png'> .\n";
+            }elseif($naipe == 1 and $c == 13) {
+                echo "<img src='img/ouro13.png'> .\n";
+            }elseif ($naipe == 2 and $c == 1) {
+                echo "<img src='img/copas1.png'> .\n";
+            }elseif($naipe == 2 and $c == 2) {
+                 echo "<img src='img/copas2.png'> .\n";
+            }elseif($naipe == 2 and $c == 3) {
+                echo "<img src='img/copas3.png'> .\n";
+            }elseif($naipe == 2 and $c == 4) {
+                echo "<img src='img/copas4.png'> .\n";
+            }elseif($naipe == 2 and $c == 5) {
+                echo "<img src='img/copas5.png'> .\n";
+            }elseif($naipe == 2 and $c == 6) {
+                echo "<img src='img/copas6.png'> .\n";
+            }elseif($naipe == 2 and $c == 7) {
+                echo "<img src='img/copas7.png'> .\n";
+            }elseif($naipe == 2 and $c == 8) {
+                echo "<img src='img/copas8.png'> .\n";
+            }elseif($naipe == 2 and $c == 9) {
+                echo "<img src='img/copas9.png'> .\n";
+            }elseif($naipe == 2 and $c == 10) {
+                echo "<img src='img/copas10.png'> .\n";
+            }elseif($naipe == 2 and $c == 11) {
+                echo "<img src='img/copas11.png'> .\n";
+            }elseif($naipe == 2 and $c == 12) {
+                echo "<img src='img/copas12.png'> .\n";
+            }elseif($naipe == 2 and $c == 13) {
+                echo "<img src='img/copas13.png'> .\n";
+            }elseif ($naipe == 3 and $c == 1) {
+                echo "<img src='img/paus1.png'> .\n";
+            }elseif($naipe == 3 and $c == 2) {
+                 echo "<img src='img/paus2.png'> .\n";
+            }elseif($naipe == 3 and $c == 3) {
+                echo "<img src='img/paus3.png'> .\n";
+            }elseif($naipe == 3 and $c == 4) {
+                echo "<img src='img/paus4.png'> .\n";
+            }elseif($naipe == 3 and $c == 5) {
+                echo "<img src='img/paus5.png'> .\n";
+            }elseif($naipe == 3 and $c == 6) {
+                echo "<img src='img/paus6.png'> .\n";
+            }elseif($naipe == 3 and $c == 7) {
+                echo "<img src='img/paus7.png'> .\n";
+            }elseif($naipe == 3 and $c == 8) {
+                echo "<img src='img/paus8.png'> .\n";
+            }elseif($naipe == 3 and $c == 9) {
+                echo "<img src='img/paus9.png'> .\n";
+            }elseif($naipe == 3 and $c == 10) {
+                echo "<img src='img/paus10.png'> .\n";
+            }elseif($naipe == 3 and $c == 11) {
+                echo "<img src='img/paus11.png'> .\n";
+            }elseif($naipe == 3 and $c == 12) {
+                echo "<img src='img/paus12.png'> .\n";
+            }elseif($naipe == 3 and $c == 13) {
+                echo "<img src='img/paus13.png'> .\n";
+            }elseif ($naipe == 4 and $c == 1) {
+                echo "<img src='img/espadas1.png'> .\n";
+            }elseif($naipe == 4 and $c == 2) {
+                 echo "<img src='img/espadas2.png'> .\n";
+            }elseif($naipe == 4 and $c == 3) {
+                echo "<img src='img/espadas3.png'> .\n";
+            }elseif($naipe == 4 and $c == 4) {
+                echo "<img src='img/espadas4.png'> .\n";
+            }elseif($naipe == 4 and $c == 5) {
+                echo "<img src='img/espadas5.png'> .\n";
+            }elseif($naipe == 4 and $c == 6) {
+                echo "<img src='img/espadas6.png'> .\n";
+            }elseif($naipe == 4 and $c == 7) {
+                echo "<img src='img/espadas7.png'> .\n";
+            }elseif($naipe == 4 and $c == 8) {
+                echo "<img src='img/espadas8.png'> .\n";
+            }elseif($naipe == 4 and $c == 9) {
+                echo "<img src='img/espadas9.png'> .\n";
+            }elseif($naipe == 4 and $c == 10) {
+                echo "<img src='img/espadas10.png'> .\n";
+            }elseif($naipe == 4 and $c == 11) {
+                echo "<img src='img/espadas11.png'> .\n";
+            }elseif($naipe == 4 and $c == 12) {
+                echo "<img src='img/espadas12.png'> .\n";
+            }elseif($naipe == 4 and $c == 13) {
+                echo "<img src='img/espadas13.png'> .\n";
+            }
+        }
     }
     ?>
 </body>
--- index.php
+++ index.php
@@ -27,6 +27,7 @@
        }
        </style>
     <?php 
+    include_once 'cartas.php';
     $jogador = "";
     if(isset($_POST['nome']))
        $jogador = $_POST['nome'];
@@ -34,32 +35,51 @@
     $naipe = "";
     if(isset($_POST['naipe']))
        $naipe = $_POST['naipe'];
-    
-    $cartas = "";
-       if(isset($_POST['cartas']))
-          $cartas = $_POST['cartas'];
+   
+    $num = isset($_POST['num']) ? $_POST['num'] : 0;
+
+    $acao = isset($_POST['acao']) ? $_POST['acao'] : "";
+
+    $Jogador = sortear($num);
     ?> 
 </head>
 <body>
-    <form action="cartas.php" method="post">
-    Jogador: <input type="text" name="nome" id="nome"><br><br>
-    <input type="radio" id="ouros" name="naipe" value="ouros">
-    <label for="ouros">Ouros</label><br>
-    <input type="radio" id="copas" name="naipe" value="copas">
-    <label for="copas">Copas</label><br>
-    <input type="radio" id="paus" name="naipe" value="paus">
-    <label for="paus">Paus</label><br>
-    <input type="radio" id="espadas" name="naipe" value="espadas">
-    <label for="espadas">Espadas</label><br><br>
+    <form action="" method="post">
+    Jogador: <input type="text" name="nome" id="nome" value="<?php echo $jogador ?>"><br><br>
+    
+    <input type="radio" id="ouros" name="naipe" value="1 
+       <?php if ($naipe == 1) echo 'checked';?>">Ouros <br>
+    <input type="radio" id="copas" name="naipe" value="2" 
+       <?php if ($naipe == 2) echo 'checked';?>>Copas <br>
+    <input type="radio" id="paus" name="naipe" value="3 
+       <?php if ($naipe == 3) echo 'checked';?>">Paus <br>
+    <input type="radio" id="espadas" name="naipe" value="4
+       <?php if ($naipe == 4) echo 'checked';?>">Espadas <br><br>
+
     Número de cartas desejado: <br>
-    <select name="cartas">
-       <option value="2">2</option>
-       <option value="3">3</option>
-       <option value="4">4</option>
-       <option value="5">5</option>
-       <option value="6">6</option>
+    <select name="num" id="num">
+    <option value="2" <?php if ($num == 2) echo 'selected'; ?>>2</option>
+    <option value="3" <?php if ($num == 3) echo 'selected'; ?>>3</option>
+    <option value="4" <?php if ($num == 4) echo 'selected'; ?>>4</option>
+    <option value="5" <?php if ($num == 5) echo 'selected'; ?>>5</option>
+    <option value="6" <?php if ($num == 6) echo 'selected'; ?>>6</option>
     </select><br><br>
-    <button type="submit">Próximo<a href="cartas.php"></a></button>
+    <?php
+    echo "$jogador: <br>" ;
+    for ($i=0; $i < $num; $i++) { 
+        echo "<input type='hidden' name='h".($i+1)."' value='".$Jogador[$i]."'>";
+     }
+    ?>
+    
+    <button type="submit" name="acao" id="acao" value="sortear">Sortear</button>
+    <button type="submit" name="acao" id="acao" value="jogar">Jogar</button>
     </form>
+    <?php
+    if ($acao == "jogar") {
+         
+    }elseif ($acao == "sortear") {
+        mostrarCartas($Jogador, $naipe);
+    }
+    ?>
 </body>
 </html>
